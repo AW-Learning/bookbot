ebbb352ba6a2730157a8576e83241c8a8d8ee8f8
Adub <me@alexwhite.digital>
2025-04-21 16:43:56 -0500

    feat(stats): add character count function

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from stats import get_num_words
+from stats import get_num_words 
+from stats import get_num_char
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    words = get_num_words(text)
-    print(f"{words} words found in the document")
+    words = get_num_char(text)
+    print(words)
 
 def get_book_text(path):
     with open(path) as f:
--- stats.py
+++ stats.py
@@ -4,3 +4,22 @@ def get_num_words(text1):
     for word in word_list:
         word_count += 1
     return word_count
+
+def get_num_char(text1):
+    char_list = set()
+    total_count = {}
+    file = list(text1.lower())
+    for char in file:
+        char_list.add(char)
+    for ch in char_list:
+        count = 0
+        for char in file:
+            if char == ch:
+                count += 1
+        total_count[ch] = count
+    '''
+    for word in word_list:
+        for char in word:
+            char_list.add(char.lower())
+    '''
+    return total_count
